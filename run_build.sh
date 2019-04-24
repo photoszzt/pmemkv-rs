224b598238eb385657d456a913322603694092d9
Zhiting Zhu <zhitingz@cs.utexas.edu>
2019-04-24 11:39:49 -0500

    fix path

--- run_build.sh
+++ run_build.sh
@@ -4,15 +4,16 @@ wget https://raw.githubusercontent.com/rust-lang/rustup.rs/master/rustup-init.sh
 chmod u+x rustup-init.sh
 ./rustup-init.sh -y
 export PATH=$HOME/.cargo/bin:$PATH
-git clone https://github.com/pmem/pmemkv
-cd pmemkv
+git clone https://github.com/pmem/pmemkv pmemkv_src
+cd pmemkv_src
 mkdir bin
 cd bin
 cmake .. -DCMAKE_BUILD_TYPE=Release \
     -DTBB_DIR=/opt/tbb/cmake \
     -DCMAKE_INSTALL_PREFIX=/usr/local
 make
-echo pass | sudo -S make install
+echo pass | sudo -S cp libpmemkv.so /usr/local/lib/
+echo pass | sudo -S cp ../src/pmemkv.h /usr/local/include/libpmemkv.h
 cd /pmemkv-rs
 cargo build --verbose --all
 cargo test --verbose --all
