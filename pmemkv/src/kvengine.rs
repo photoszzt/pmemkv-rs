e94d3fe12b47af64a6d0b7f88b890315d4eb8f5a
Zhiting Zhu <zhitingz@cs.utexas.edu>
2019-04-25 02:15:59 -0500

    basic binding done

--- pmemkv/src/kvengine.rs
+++ pmemkv/src/kvengine.rs
@@ -1,6 +1,8 @@
 use crate::errors::*;
-use pmemkv_sys::pmemkvapi::{KVEngine as KVEngineSys, KVStartFailureCallback};
+use pmemkv_sys::pmemkvapi::KVEngine as KVEngineSys;
+use pmemkv_sys::pmemkvapi::*;
 use std::ffi::CString;
+use std::os::raw::c_void;
 
 #[derive(Debug, Clone)]
 pub struct KVEngine(*mut KVEngineSys);
@@ -10,7 +12,7 @@ impl KVEngine {
         context: *mut ::std::os::raw::c_void,
         engine: String,
         config: String,
-        callback: KVStartFailureCallback,
+        callback: pmemkv_sys::pmemkvapi::KVStartFailureCallback,
     ) -> Result<KVEngine> {
         use pmemkv_sys::pmemkvapi::kvengine_start;
         let engine_str = CString::new(engine)?;
@@ -63,5 +65,175 @@ impl KVEngine {
         } else {
             Err(ErrorKind::NotFound(key).into())
         }
+    }
+
+    pub fn get(
+        &self,
+        context: *mut c_void,
+        key: String,
+        cb: pmemkv_sys::pmemkvapi::KVGetCallback,
+    ) -> Result<()> {
+        let key_str = CString::new(key)?;
+        unsafe {
+            pmemkv_sys::pmemkvapi::kvengine_get(
+                self.0,
+                context,
+                key_str.to_bytes().len() as i32,
+                key_str.as_ptr(),
+                cb,
+            )
+        };
+        Ok(())
+    }
+
+    pub fn exists(&self, key: String) -> Result<()> {
+        let key_str = CString::new(key.clone())?;
+        let res = unsafe {
+            pmemkv_sys::pmemkvapi::kvengine_exists(
+                self.0,
+                key_str.to_bytes().len() as i32,
+                key_str.as_ptr(),
+            )
+        };
+        if res == 0 {
+            Ok(())
+        } else if res == -1 {
+            Err(ErrorKind::Fail.into())
+        } else {
+            Err(ErrorKind::NotFound(key).into())
+        }
+    }
+
+    pub fn each(&self, context: *mut c_void, cb: KVEachCallback) {
+        unsafe { kvengine_each(self.0, context, cb) }
+    }
+
+    pub fn each_above(&self, context: *mut c_void, key: String, cb: KVEachCallback) -> Result<()> {
+        let key_str = CString::new(key)?;
+        unsafe {
+            kvengine_each_above(
+                self.0,
+                context,
+                key_str.to_bytes().len() as i32,
+                key_str.as_ptr(),
+                cb,
+            )
+        }
+        Ok(())
+    }
+
+    pub fn each_below(&self, context: *mut c_void, key: String, cb: KVEachCallback) -> Result<()> {
+        let key_str = CString::new(key)?;
+        unsafe {
+            kvengine_each_below(
+                self.0,
+                context,
+                key_str.to_bytes().len() as i32,
+                key_str.as_ptr(),
+                cb,
+            )
+        }
+        Ok(())
+    }
+
+    pub fn each_between(
+        &self,
+        context: *mut c_void,
+        key1: String,
+        key2: String,
+        cb: KVEachCallback,
+    ) -> Result<()> {
+        let key1_str = CString::new(key1)?;
+        let key2_str = CString::new(key2)?;
+        unsafe {
+            kvengine_each_between(
+                self.0,
+                context,
+                key1_str.to_bytes().len() as i32,
+                key1_str.as_ptr(),
+                key2_str.to_bytes().len() as i32,
+                key2_str.as_ptr(),
+                cb,
+            )
+        }
+        Ok(())
+    }
+
+    pub fn count(&self) -> i64 {
+        unsafe { kvengine_count(self.0) }
+    }
+
+    pub fn count_above(&self, key: String) -> Result<i64> {
+        let key_str = CString::new(key)?;
+        Ok(unsafe {
+            kvengine_count_above(self.0, key_str.to_bytes().len() as i32, key_str.as_ptr())
+        })
+    }
+
+    pub fn count_below(&self, key: String) -> Result<i64> {
+        let key_str = CString::new(key)?;
+        Ok(unsafe {
+            kvengine_count_below(self.0, key_str.to_bytes().len() as i32, key_str.as_ptr())
+        })
+    }
+
+    pub fn count_between(&self, key1: String, key2: String) -> Result<i64> {
+        let key1_str = CString::new(key1)?;
+        let key2_str = CString::new(key2)?;
+        Ok(unsafe {
+            kvengine_count_between(
+                self.0,
+                key1_str.to_bytes().len() as i32,
+                key1_str.as_ptr(),
+                key2_str.to_bytes().len() as i32,
+                key2_str.as_ptr(),
+            )
+        })
+    }
+
+    pub fn all(&mut self, context: *mut c_void, cb: KVAllCallback) {
+        unsafe { kvengine_all(self.0, context, cb) }
+    }
+
+    pub fn all_above(&mut self, context: *mut c_void, key: String, cb: KVAllCallback) -> Result<()> {
+        let key_str = CString::new(key)?;
+        Ok(unsafe {
+            kvengine_all_above(
+                self.0,
+                context,
+                key_str.to_bytes().len() as i32,
+                key_str.as_ptr(),
+                cb,
+            )
+        })
+    }
+
+    pub fn all_below(&mut self, context: *mut c_void, key: String, cb: KVAllCallback) -> Result<()> {
+        let key_str = CString::new(key)?;
+        Ok(unsafe {
+            kvengine_all_below(
+                self.0,
+                context,
+                key_str.to_bytes().len() as i32,
+                key_str.as_ptr(),
+                cb,
+            )
+        })
+    }
+
+    pub fn all_between(&mut self, context: *mut c_void, key1: String, key2: String, cb: KVAllCallback) -> Result<()> {
+        let key1_str = CString::new(key1)?;
+        let key2_str = CString::new(key2)?;
+        Ok(unsafe {
+            kvengine_all_between(
+                self.0,
+                context,
+                key1_str.to_bytes().len() as i32,
+                key1_str.as_ptr(),
+                key2_str.to_bytes().len() as i32,
+                key2_str.as_ptr(),
+                cb,
+            )
+        })
     }
 }
